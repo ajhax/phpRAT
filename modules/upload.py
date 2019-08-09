a373fd443c5056cae068606e52aaee09096df3d5
Jatin Luthra <jatinluthra14@gmail.com>
2019-08-09 17:04:17 +0530

    Added missing path option in upload module

--- modules/upload.py
+++ modules/upload.py
@@ -2,7 +2,9 @@ class upload:
     def __init__(self):
         self.name = 'upload'
         self.machine_id = None
+        self.path = None
 
     @property
     def options(self):
-        return [('machine_id', self.machine_id, 'True')]
+        return [('machine_id', self.machine_id, 'True'),
+                ('path', self.path, 'True')]
